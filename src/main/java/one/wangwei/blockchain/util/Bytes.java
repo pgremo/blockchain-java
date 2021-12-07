06b79041c47511617428797557b45514e9680c53
Patrick Gremo <patrick.gremo@gmail.com>
2021-12-07 11:38:10 -0600

    match naming

--- src/main/java/one/wangwei/blockchain/util/Bytes.java
+++ src/main/java/one/wangwei/blockchain/util/Bytes.java
@@ -1,10 +1,10 @@
 package one.wangwei.blockchain.util;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static java.lang.Character.digit;
 import static java.nio.charset.StandardCharsets.US_ASCII;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * 字节数组工具类
@@ -21,13 +21,13 @@ public class Bytes {
     private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(US_ASCII);
 
     public static String byteArrayToHex(byte[] value) {
-        final byte[] hexChars = new byte[value.length * 2];
+        final byte[] data = new byte[value.length * 2];
         for (int j = 0; j < value.length; j++) {
             final int v = value[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+            data[j * 2] = HEX_ARRAY[v >>> 4];
+            data[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
-        return new String(hexChars, StandardCharsets.UTF_8);
+        return new String(data, UTF_8);
     }
 
     public static Optional<byte[]> hexToByteArray(String value) {
